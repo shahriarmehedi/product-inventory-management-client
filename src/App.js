73e7fc786948334ddba7e36ec12df8804d5f5d7e
Farshid-997 <arnob.farshid@gmail.com>
2022-01-03 21:07:14 +0600

    dashboard created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import AuthProvider from './context/AuthProvider';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+ 
+} from "react-router-dom";
+import Dashboard from './Component/Dashboard/Dashboard';
 function App() {
   return (
     <div className="App">
       <AuthProvider>
+        <Router>
+          <Switch>
 
+      
+          <Route path="/dashboard">
+<Dashboard></Dashboard>
+          </Route>
+
+          
+          </Switch>
+        </Router>
+     
       </AuthProvider>
+
+
     </div>
   );
 }
---
+++ src/Component/Dashboard/MakeAdmin/MakeAdmin.js
@@ -0,0 +1,46 @@
+import { Alert, Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
+
+const MakeAdmin = () => {
+    const [email,setEmail]=useState('')
+    const [success,setSuccess]=useState(false)
+    const handleOnBlur=e=>{
+        setEmail(e.target.value)
+    }
+    const habdleAdminSubmit=e=>{
+        e.preventDefault()
+        const user={email}
+        fetch('http://locahost/5000/users/admin',{
+            method:'PUT',
+            headers:{
+                'content-type':'application/json'
+              },
+              body:JSON.stringify(user)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.modifiedCount)
+            console.log(data)
+            setSuccess(true)
+        })
+    }
+    return (
+        <div>
+         <h1>Make An Admin</h1>  
+
+         <form onSubmit={habdleAdminSubmit}>
+         <TextField  
+         label="Email" 
+        
+         variant="standard" 
+         onBlur={handleOnBlur}
+         />
+          
+         <Button type="submit" variant="contained">Make Admin</Button>
+         </form>
+         {success&&<Alert>Made Admin Successfully</Alert>}
+        </div>
+    );
+};
+
+export default MakeAdmin;
