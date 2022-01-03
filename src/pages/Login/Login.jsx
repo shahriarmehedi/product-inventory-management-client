89efd9bca1577bcb2938f6ba65bf34a1a08b416e
Nafisa-Muntaha7 <farjanapopi8@gmail.com>
2022-01-04 00:27:36 +0600

    tsparticles added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,21 @@
-import { Box, Grid, TextField } from '@mui/material';
+import { Box, Container, Grid, TextField } from '@mui/material';
 import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 import image1 from '../../images/image1.jfif';
 import GoogleIcon from '@mui/icons-material/Google';
 import useAuth from '../../hooks/useAuth';
 import LoginIcon from '@mui/icons-material/Login';
+import Particles from "react-tsparticles";
 
 const Login = () => {
+    const particlesInit = (main) => {
+        console.log(main);
+        // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    };
+    const particlesLoaded = (container) => {
+        console.log(container);
+    };
+
     const { signInUsingGoogle } = useAuth();
     const location = useLocation();
     const history = useHistory();
@@ -20,12 +29,95 @@ const Login = () => {
     }
 
     return (
-        <Box className='bg-slate-900 px-20 py-40 text-white'>
+        <Box className='bg-slate-900 px-20 py-40 text-white text-center'>
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    background: {
+                        color: {
+                            value: "rgb(15 23 42)",
+                        },
+                    },
+                    fpsLimit: 60,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            bubble: {
+                                distance: 400,
+                                duration: 2,
+                                opacity: 0.8,
+                                size: 40,
+                            },
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 200,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                        links: {
+                            color: "#2789F8",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outMode: "bounce",
+                            random: false,
+                            speed: 2.5,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            random: true,
+                            value: 4,
+                        },
+                    },
+                    detectRetina: true,
+                }}
+            />
+
             <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
+                <Grid item xs={12} md={6} className='p-5'>
                     <img src={image1} width='100%' className='rounded-lg ' alt="" />
                 </Grid>
-                <Grid item xs={12} md={6} className='bg-slate-800 text-center mt-5 rounded-lg'>
+                <Grid item xs={12} md={6} className='bg-slate-800 mt-5 rounded-lg'>
                     <h2 className='text-4xl py-2'>Login</h2>
                     <h4 className='text-xl'>Manage your business with our automated inventory management system</h4>
                     <form onSubmit={handleFormSubmit}>
