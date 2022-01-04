a856a3b9019b98c6c579baab792df4dd06f78407
Farshid-997 <arnob.farshid@gmail.com>
2022-01-04 18:20:38 +0600

    registration updated

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,10 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react"
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+import { getAuth,signOut,onAuthStateChanged,signInWithPopup, GoogleAuthProvider} from "firebase/auth";
 import initializeAuthentication from "../Firebase/firebase.initialize";
 
 initializeAuthentication();
 const useFirebase = () => {
+    //const[isLoading,setIsLoading]=useState(true)
+   // const [admin,setAdmin]=useState(false)
     const [user, setUser] = useState({});
     const [error, setError] = useState('');
     const auth = getAuth();
@@ -23,6 +25,57 @@ const useFirebase = () => {
                 setError(error.message)
             })
     }
+
+//register with email and password
+
+// const registerUser=(email,password,name,history)=>{
+//     setIsLoading(true)
+//     createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+  
+//     setError('')
+//    const newUser={email,displayName:name}
+//    setUser(newUser)
+// saveUser(email,name,'POST')
+//    updateProfile(auth.currentUser, {
+//     displayName: name
+    
+//   }).catch((error) => {
+    
+//   });
+  
+//    history.replace('/dashboard')
+//   })
+//   .catch((error) => {
+  
+//     setError(error.message);
+    
+//   }).finally(()=>{
+//       setIsLoading(false)
+//   });
+// }
+
+
+//loginuser
+
+// const loginUser=(email,password,location,history)=>{
+//     setIsLoading(true)
+//     signInWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+  
+//     const destination=location?.state?.from||'/dashboard'
+//     history.replace(destination)
+//     setError('')
+  
+//   })
+//   .catch((error) => {
+  
+//     setError(error.message);
+//   }).finally(()=>{
+//     setIsLoading(false)
+// });
+
+// }
     ////////// USER LOG OUT //////////
     const logOut = () => {
         signOut(auth)
@@ -40,6 +93,26 @@ const useFirebase = () => {
             }
         })
     }, [])
+
+    // useEffect(()=>{
+
+    //     fetch(`http://localhost:5000/users/${user.email}`)
+    //     .then(res=>res.json())
+    //     .then(data=>setAdmin(data.admin))
+    //     },[user.email])
+
+    // const saveUser=(email,displayName,method)=>{
+
+    //     const user={email,displayName}
+    //     fetch('http://localhost:5000/users',{
+    //       method:method,
+    //       headers:{
+    //         'content-type':'application/json'
+    //       },
+    //       body:JSON.stringify(user)
+    //     })
+    //     .then()
+    //   }
 
     return {
         signInUsingGoogle, user, error, logOut
