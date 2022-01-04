40567d5378643306f953c94f875dc3f25357851d
Farshid-997 <arnob.farshid@gmail.com>
2022-01-04 23:53:31 +0600

    product shows

--- src/Component/Dashboard/AdminDashboard/AdminDashboard.js
+++ src/Component/Dashboard/AdminDashboard/AdminDashboard.js
@@ -23,30 +23,28 @@ const AdminDashboard = (props) => {
             <td>{productImageURL}</td>
           </tr>
           <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
+          <td>{productName}</td>
+            <td>{productCategory}</td>
+            <td>{productPrice}</td>
+            <td>{productQuantity}</td>
+            <td>{productImageURL}</td>
           </tr>
           <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
+          <td>{productName}</td>
+            <td>{productCategory}</td>
+            <td>{productPrice}</td>
+            <td>{productQuantity}</td>
+            <td>{productImageURL}</td>
           </tr>
           <tr>
-            <td>Island Trading</td>
-            <td>Helen Bennett</td>
-            <td>UK</td>
+          <td>{productName}</td>
+            <td>{productCategory}</td>
+            <td>{productPrice}</td>
+            <td>{productQuantity}</td>
+            <td>{productImageURL}</td>
           </tr>
-          <tr>
-            <td>Laughing Bacchus Winecellars</td>
-            <td>Yoshi Tannamuri</td>
-            <td>Canada</td>
-          </tr>
-          <tr>
-            <td>Magazzini Alimentari Riuniti</td>
-            <td>Giovanni Rovelli</td>
-            <td>Italy</td>
-          </tr>
+      
+        
         </tbody>
       </table>
     </div>
